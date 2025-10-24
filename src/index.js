ff41d0080226f4cd539bdc0517bb0e663c3f98d2
MASTEROCH <34756543+MASTEROCH@users.noreply.github.com>
2025-10-25 01:16:12 +0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NewApp from './NewApp';
+import './style.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <NewApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
